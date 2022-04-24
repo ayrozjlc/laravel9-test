ed81a4846650832041b887b9be914181dd632cd7
José López Chávez <ayroz.jlc@gmail.com>
2022-04-24 16:21:55 -0500

    migrate empleados

---
+++ database/migrations/2022_04_24_115128_create_empleados_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('empleados', function (Blueprint $table) {
+            $table->id();
+            $table->string('nombre', 30);
+            $table->string('apellidos', 100)->nullable();
+            $table->string('correo', 70)->unique();
+            $table->double('salario_diario', 12, 2);
+            $table->smallInteger('puesto');
+            $table->smallInteger('estatus');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('empleados');
+    }
+};
